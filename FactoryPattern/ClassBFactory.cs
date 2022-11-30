2ef1ee9b36f9690f881c60485dc479c3fb04f856
merts <merts@MERT>
2022-11-30 22:18:29 +0300

    Add project files.

---
+++ FactoryPattern/ClassBFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ClassBFactory : IFactory
+    {
+        public Class Create()
+        {
+            Class @class = new ClassB();
+            return @class;
+        }
+
+    }
+}
